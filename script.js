c8b812ebc328426a07917e66a72a6158cd00048d
AustineJ <austinejumah001@gmail.com>
2025-01-23 17:21:29 +0300

    Initial Commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
   const form = document.getElementById("registration-form");
   const feedbackDiv = document.getElementById("form-feedback");
 
@@ -11,39 +11,33 @@ document.addEventListener("DOMContentLoaded", function(){
     let isValid = true;
     const messages = [];
 
-    feedbackDiv.style.display = "block";
     // Username Validation
-
-    if(username.length < 3){
+    if (username.length < 3) {
       isValid = false;
       messages.push("Username should be at least 3 characters");
     }
 
     // Email Validation
-
-    if(!email.includes("@") && !email.includes(".")){
+    if (!email.includes("@") || !email.includes(".")) {
       isValid = false;
       messages.push("Invalid email address");
     }
 
     // Password Validation
-    if(password.length < 8){
+    if (password.length < 8) {
       isValid = false;
       messages.push("Password should contain at least 8 characters");
     }
 
-  
-    if(isValid){
+    feedbackDiv.style.display = "block";
+    if (isValid) {
       feedbackDiv.style.color = "#28a745";
-      feedbackDiv.textContent = "Registration Successful";
-    }else{
-      const errorMessage = `<p>${messages.join(`<br>`)}</p>`
-      feedbackDiv.innerHTML = errorMessage;
+      feedbackDiv.textContent = "Registration successful!";
+    } else {
+      feedbackDiv.innerHTML = messages.join("<br>");
       feedbackDiv.style.color = "#dc3545";
     }
+  });
+});
 
-    
-  
-  }) 
-})
 
